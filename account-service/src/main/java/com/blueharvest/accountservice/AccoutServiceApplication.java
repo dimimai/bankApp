07f69922dffbd1cd59c2920211058bbcb3a57bd1
Dimi <dimi_mai@hotmail.com>
2019-06-08 18:10:36 +0200

    Project initialization

---
+++ account-service/src/main/java/com/blueharvest/accountservice/AccoutServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blueharvest.accountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccoutServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccoutServiceApplication.class, args);
+	}
+
+}
