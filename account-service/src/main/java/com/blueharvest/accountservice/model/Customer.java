89f9ce24cf9e510aa4d517f6b245acb5dc4759c0
Dimi <dimi_mai@hotmail.com>
2019-06-08 19:13:19 +0200

    fix: change generationType in Customer class

--- account-service/src/main/java/com/blueharvest/accountservice/model/Customer.java
+++ account-service/src/main/java/com/blueharvest/accountservice/model/Customer.java
@@ -16,7 +16,7 @@ import java.util.Date;
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     private String firstName;
